a466ccf5d13b36db974041aaa96a335a7091b3b9
Numan Abubakar <bhalonoman@gmail.com>
2023-10-21 01:56:29 +0500

    f-string

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello Python!")
+name:str = "Numan Abubakar"
+father_name :str = "Muhammad Abubakar"
+age:int = 21
+
+
+card:str = f"""
+My Information
+Name : {name}
+Father Name : {father_name}
+age : {age}
+"""
+print(card)
