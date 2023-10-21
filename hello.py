649202f7f63d4ed1aa117d492ca0b98bdeb0205f
Numan Abubakar <bhalonoman@gmail.com>
2023-10-21 23:29:53 +0500

    string method

--- hello.py
+++ hello.py
@@ -2,17 +2,25 @@ name: str = "Numan Abubakar"
 father_name: str = "Muhammad Abubakar"
 age: int = 21
 
-
-# card:str = f"""
-# My Information
-# Name : {name}
-# Father Name : {father_name}
-# age : {age}
-# """
+# f-string
+card2: str = f"""
+My Information
+Name: {name}
+Father Name: {father_name}
+age: {age}
+"""
+# without f-string
 card: str = """
 My Information
 Name : {}
 Father Name : {}
 age : {}
 """.format(name, father_name, age)
-print(card)
+# print(card)
+
+message: str = f""" Hello {name}, Hope you are doing best! """
+# print(message)
+name_lstrip = "                  numan abubakar          "
+print(name_lstrip.lstrip())
+print(name_lstrip.rstrip())
+print(name_lstrip.strip())
